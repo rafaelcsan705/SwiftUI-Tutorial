332c2ffa8449e5f890a9183e287db6aee68a0f4d
Rafael C Santos <rafaelcsan705@gmail.com>
2024-06-19 20:08:40 +0100

    Drawing and animation
    Animating views and transitions
    https://developer.apple.com/tutorials/swiftui/animating-views-and-transitions

---
+++ Landmark/Views/Badges/RotatedBadgeSymbol.swift
@@ -0,0 +1,21 @@
+//
+//  RotatedBadgeSymbol.swift
+//  Landmark
+//
+//  Created by Rafael Santos on 19/06/2024.
+//
+
+import SwiftUI
+
+struct RotatedBadgeSymbol: View {
+    let angle: Angle
+    var body: some View {
+        BadgeSymbol()
+            .padding(-60)
+            .rotationEffect(angle, anchor: .bottom)
+    }
+}
+
+#Preview {
+    RotatedBadgeSymbol(angle: Angle(degrees: 5))
+}
