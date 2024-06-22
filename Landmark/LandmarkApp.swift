249ca9ae406ec495e3a18b97f9fd012ac4d96d80
Rafael C Santos <rafaelcsan705@gmail.com>
2024-06-22 13:48:32 +0100

    Framework integration
    Creating a watchOS app

--- Landmark/LandmarkApp.swift
+++ Landmark/LandmarkApp.swift
@@ -8,15 +8,15 @@
 import SwiftUI
 
 @main
-struct LandmarkApp: App {
+struct LandmarksApp: App {
     @State private var modelData = ModelData()
-    
+
     var body: some Scene {
         WindowGroup {
             ContentView()
                 .environment(modelData)
         }
-        
+
         #if os(watchOS)
         WKNotificationScene(controller: NotificationController.self, category: "LandmarkNear")
         #endif
--- WatchLandmarks Watch App/Notifications/NotificationController.swift
+++ WatchLandmarks Watch App/Notifications/NotificationController.swift
@@ -5,12 +5,11 @@
 //  Created by Rafael Santos on 22/06/2024.
 //
 
-import Foundation
+import WatchKit
 import SwiftUI
 import UserNotifications
 
 class NotificationController: WKUserNotificationHostingController<NotificationView> {
-    
     var landmark: Landmark?
     var title: String?
     var message: String?
@@ -18,20 +17,30 @@ class NotificationController: WKUserNotificationHostingController<NotificationVi
     let landmarkIndexKey = "landmarkIndex"
     
     override var body: NotificationView {
-        NotificationView(title: title, message: message, landmark: landmark)
+        NotificationView(
+            title: title,
+            message: message,
+            landmark: landmark
+        )
     }
-    
+
     override func didReceive(_ notification: UNNotification) {
         let modelData = ModelData()
-        
-        let notificatioData = notification.request.content.userInfo as? [String: Any]
-        let aps = notificatioData?["aps"] as? [String: Any]
+
+
+        let notificationData =
+            notification.request.content.userInfo as? [String: Any]
+
+
+        let aps = notificationData?["aps"] as? [String: Any]
         let alert = aps?["alert"] as? [String: Any]
-        
+
+
         title = alert?["title"] as? String
         message = alert?["body"] as? String
-        
-        if let index = notificatioData?[landmarkIndexKey] as? Int {
+
+
+        if let index = notificationData?[landmarkIndexKey] as? Int {
             landmark = modelData.landmarks[index]
         }
     }
